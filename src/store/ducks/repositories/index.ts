1c1c2db2fb857c673692015b2fd005d9dc588ffa
Arthur Grigoletto <arthur.grigoletto.lima@gmail.com>
2019-04-03 21:45:52 -0300

    First Commit

---
+++ src/store/ducks/repositories/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Action Types
+ */
+
+export enum RepositoriesTypes {
+  LOAD_REQUEST = '@repositories/LOAD_REQUEST',
+  LOAD_SUCCESS = '@repositories/LOAD_SUCCESS',
+  LOAD_FAILURE = '@repositories/LOAD_FAILURE',
+}
